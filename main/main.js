cdb46ef4d78ca0c9f3c89fecd49361a43bcee599
HuShuangshuang2017 <1404879554@qq.com>
2017-06-22 18:15:46 +0800

    test2

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
-    document.write("</b>请按第一位数大于第二位数输入</b>");
+    console.log("</b>请按第一位数大于第二位数输入</b>");
 	var a1=prompt("请输入第一位数:","0");
 	var a2=prompt("请输入第一位数:","0");
 	var qr=a1%a2;
-	document.write("给定"+a1+"和"+a2+","+"返回"+qr);
+	console.log("给定"+a1+"和"+a2+","+"返回"+qr);
 };
